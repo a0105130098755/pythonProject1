3a896f6da8083cc6b9074040d8cb993fdb307498
DESKTOP-OLBQKOO\user1 <a01051300987@gmail.com>
2024-03-18 17:42:50 +0900

    03/18 python

---
+++ 기본연습.py
@@ -0,0 +1,17 @@
+print("Hello Python")
+print(100)
+print(100+200)
+
+name = "고문석"
+print(name)
+
+addr = "익산시 마동"
+position = "학생"
+print(f"""
+주소:{addr}
+직책:{position}
+""")
+
+print(38)
+print("무자열 출력")
+print([1,2,3])
